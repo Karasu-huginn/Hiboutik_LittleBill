741870d331f0c7c9366bce96bc190c4cedfb7243
erwann quin <erwann.quin@supinfo.com>
2025-10-22 19:06:43 +0200

    feat: customer search endpoint + connector base

--- hiboutik_connector.py
+++ hiboutik_connector.py
@@ -1,2 +1,30 @@
+import base64
 import requests
+import json
 
+with open("creds.json", "r") as file:
+    creds = json.loads(file.read())
+
+API_URL = f"https://{creds["account"]}.hiboutik.com/api"
+
+def gen_token():
+    string = f"{creds["api_login"]}:{creds["api_key"]}"
+    string_bytes = string.encode("ascii")
+    b64_b = base64.b64encode(string_bytes)
+    b64_str = b64_b.decode("ascii")
+    return b64_str
+
+def get_customer_sales(customer_id):
+    token = gen_token()
+    url = f"{API_URL}/sales/search?customer_id={customer_id}"
+    headers = {"Accept": "*/*", "Authorization": "Basic "+token}
+    sales = requests.get(url, headers=headers)
+    return sales.json()
+
+def get_customer(params):
+    token = gen_token()
+    url = f"{API_URL}/customers/search?{params}"
+    print(url)
+    headers = {"Accept": "*/*", "Authorization": "Basic "+token}
+    customers = requests.get(url, headers=headers)
+    return customers.json()
--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
+import hiboutik_connector as HC
 
 app = FastAPI()
 
 @app.get('/')
 def root():
     return {"var":"value"}
+
+@app.get("/customer/search")
+def customer_search(last_name:str="", first_name:str="", email:str="", phone:str="", country:str="", vat:str=""):
+    params = {"last_name":last_name, "first_name":first_name, "email":email, "phone":phone, "country":country, "vat":vat}
+    params = [f"{key}={value}" for key,value in params.items() if value != ""] #* deletes empty params
+    params_str = "&".join(params)
+    return HC.get_customer(params_str)
