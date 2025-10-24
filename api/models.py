c9c64ba0e8774e348f2ddd8a5905640603e1797d
erwann quin <erwann.quin@supinfo.com>
2025-10-25 01:57:07 +0200

    chore: set Sales & Customers attributes to nullable

--- api/models.py
+++ api/models.py
@@ -5,27 +5,27 @@ class Customers(Base):
     __tablename__ = "customers"
 
     customers_id = Column(Integer, primary_key=True, index=True)
-    last_name = Column(String, index=True)
-    first_name = Column(String, index=True)
-    email = Column(String, index=True)
-    phone = Column(String, index=True)
-    country = Column(String, index=True)
-    vat = Column(String, index=True)
+    last_name = Column(String, index=True, nullable=True)
+    first_name = Column(String, index=True, nullable=True)
+    email = Column(String, index=True, nullable=True)
+    phone = Column(String, index=True, nullable=True)
+    country = Column(String, index=True, nullable=True)
+    vat = Column(String, index=True, nullable=True)
 
 class Sales(Base):
     __tablename__ = "sales"
 
     sale_id = Column(Integer, primary_key=True, index=True)
-    vendor_id = Column(Integer)
-    customer_id = Column(Integer, ForeignKey("customers.customers_id"))
-    billing_address = Column(Integer)
-    shipping_address = Column(Integer)
-    payment = Column(String)
-    ext_ref = Column(String)
-    store_id = Column(Integer)
-    takeaway = Column(Integer)
-    resource_id = Column(Integer)
-    currency = Column(String)
+    vendor_id = Column(Integer, nullable=True)
+    customer_id = Column(Integer, ForeignKey("customers.customers_id"), nullable=True)
+    billing_address = Column(Integer, nullable=True)
+    shipping_address = Column(Integer, nullable=True)
+    payment = Column(String, nullable=True)
+    ext_ref = Column(String, nullable=True)
+    store_id = Column(Integer, nullable=True)
+    takeaway = Column(Integer, nullable=True)
+    resource_id = Column(Integer, nullable=True)
+    currency = Column(String, nullable=True)
 
 class Users(Base):
     __tablename__ = "users"
